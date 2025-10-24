4e96953a30415d2a2bf73dd87ee1154707729b77
Sepehr Sahraian <sep3831@gmail.com>
2025-10-23 17:36:20 -0700

    MINOR code reformatting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import mutagen
-from mutagen.id3 import ID3
 from pathlib import Path
+
+import mutagen
+
 
 def _input_music_directory() -> Path:
     """
@@ -10,7 +11,8 @@ def _input_music_directory() -> Path:
     """
     music_directory: None | Path = None
     while not music_directory:
-        input_text = input("Absolute path to the directory where the music files are located: ")
+        input_text = input(
+            "Absolute path to the directory where the music files are located: ")
         if Path(input_text).is_dir():
             music_directory = Path(input_text)
             break
@@ -24,6 +26,7 @@ class Music:
     """
     Represents a music file
     """
+
     def __init__(self, path: Path) -> None:
         self.path: Path = path
         self.file = mutagen.File(path)
@@ -33,10 +36,12 @@ class Music:
         self.year: str = self.file['TDRC']
         self.length: int = self.file.info.length
 
+
 class Track_List:
     """
     Represents a track list
     """
+
     def __init__(self, list_path: Path) -> None:
         self._path: Path = list_path
         self._report_file: Path = self._path / "report.txt"
@@ -63,14 +68,12 @@ class Track_List:
         music.path = target
         return target
 
-
     def process_music_files(self) -> None:
         """Renames music files and orders music files"""
         for n in range(len(self._music_list)):
             music = self._music_list[n]
-            new_title: str = f'{n+1}. {music.title} - {music.album} - {music.year}'
+            new_title: str = f'{n + 1}. {music.title} - {music.album} - {music.year}'
             self._rename_music(music, new_title)
-
 
     def generate_report(self):
         with open(self._path / "track-list.txt", "w") as file:
@@ -83,7 +86,7 @@ class Track_List:
             file.write('\n')
             for n in range(len(self._music_list)):
                 music = self._music_list[n]
-                file.write(f'{n+1}. {music.title}\n')
+                file.write(f'{n + 1}. {music.title}\n')
                 file.write(f'  Artist: {music.artist}\n')
                 file.write(f'  Album: {music.album}\n')
                 file.write(f'  Year: {music.year}\n')
@@ -96,9 +99,6 @@ def main():
     track_list.process_music_files()
     track_list.generate_report()
 
+
 if __name__ == "__main__":
     main()
-
-
-
-
